2c7c2ca8f821db18d4b8588d8ab21e80d5f5eb16
EstefaniaDelgado <teff_64@hotmail.com>
2025-01-22 09:24:00 -0500

    feature(tailwind.config): add typography

--- src/views/Home/components/Testimonials/Testimonials.jsx
+++ src/views/Home/components/Testimonials/Testimonials.jsx
@@ -5,7 +5,7 @@ const Testimonials = () => {
     <section className="h-screen text-center md:text-left  md:h-96 bg-testimonials bg-cover bg-center md:bg-top bg-no-repeat">
       <article className='bg-white/40 w-full pt-5 xl:pt-10 md:w-10/12 md:bg-transparent xl:max-w-[920px] md:mx-auto'>
         <h2 className="secondHeaders">Pacientes Felices!</h2>
-        <p className="font-normal">
+        <p className="font-normal font-poppins">
           Testimonios que demuestran la calidad y compromiso con nuestros
           pacientes
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors:{
         primary:'#234A6B',
         btns:'#546FEA'
