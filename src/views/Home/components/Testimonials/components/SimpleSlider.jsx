78ab861c7572547d5da342d984f0620a955ca446
EstefaniaDelgado <teff_64@hotmail.com>
2025-01-23 13:40:07 -0500

    refactor(testimonials): add avatars and bg with opacity

--- src/views/Home/components/Testimonials/components/SimpleSlider.jsx
+++ src/views/Home/components/Testimonials/components/SimpleSlider.jsx
@@ -2,6 +2,10 @@ import Slider from 'react-slick';
 import { Box, Typography, Card, CardContent, Avatar } from '@mui/material';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Patient1 from '../../../../../assets/patient1.avif'
+import Patient2 from '../../../../../assets/patient2.avif'
+import Patient3 from '../../../../../assets/patient3.avif'
+import Patient4 from '../../../../../assets/patient4.avif'
 
 const testimonials = [
   {
@@ -9,28 +13,28 @@ const testimonials = [
     name: 'Michael',
     role: 'Paciente',
     feedback: 'Asombrosa atención y resultados.',
-    avatar: 'https://via.placeholder.com/100',
+    avatar: Patient1,
   },
   {
     id: 2,
     name: 'Anna',
     role: 'Paciente',
     feedback: 'El mejor tratamiento que he recibido.',
-    avatar: 'https://via.placeholder.com/100',
+    avatar: Patient2,
   },
   {
     id: 3,
     name: 'John',
     role: 'Paciente',
     feedback: 'Muy profesional y amable.',
-    avatar: 'https://via.placeholder.com/100',
+    avatar: Patient3,
   },
   {
     id: 4,
     name: 'Sarah',
     role: 'Paciente',
     feedback: '¡Altamente recomendado!',
-    avatar: 'https://via.placeholder.com/100',
+    avatar: Patient4,
   },
 ];
 
@@ -61,10 +65,10 @@ const SimpleSlider = () => {
   return (
     <Box
       sx={{
-        maxWidth: 1024,
+        maxWidth: 1300,
         mx: 'auto',        
       }}
-     className='px-3 py-5'
+     className='p-5'
     >
       <Slider {...settings}>
         {testimonials.map((testimonial) => (
@@ -74,23 +78,26 @@ const SimpleSlider = () => {
                 boxShadow: 3,
                 borderRadius: 2,
                 textAlign: 'center',
+                minHeight:"240px",
+                backgroundColor:'rgba(255, 255, 255, 0.8)'
               }}
             >
-              <CardContent>
+              <CardContent className='bg-white/10'>
                 <Avatar
                   src={testimonial.avatar}
                   alt={testimonial.name}
                   sx={{ width: 80, height: 80, mx: 'auto', mb: 2 }}
                 />
-                <Typography variant="h6">{testimonial.name}</Typography>
+                <Typography variant="h6" className='font-poppins'>{testimonial.name}</Typography>
                 <Typography
                   variant="subtitle2"
                   color="text.secondary"
                   sx={{ mb: 2 }}
+                  className='font-poppins'
                 >
                   {testimonial.role}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="text.secondary" className='font-poppins'>
                   {testimonial.feedback}
                 </Typography>
               </CardContent>
