e15f7eceec29d9b97074b2c710b1f8338efd7499
Carlos <carlosciccone1@gmail.com>
2025-01-24 17:05:15 -0400

    Secciones home

--- src/layouts/LayoutLadingPage.jsx
+++ src/layouts/LayoutLadingPage.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom"
 import CustomFooter from "../components/CustomFooter"
-import NavBar from "../components/navbar"
 
 const LayoutLadingPage = () => {
   return (
     <div className="mx-auto min-h-screen flex flex-col">
-      <NavBar/>
       <main className="flex-1">
         <Outlet/>
       </main>
