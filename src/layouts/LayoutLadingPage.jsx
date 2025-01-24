58bfa2a4e0ed2072850e721741f94f5c1b4be43e
Carlos <carlosciccone1@gmail.com>
2025-01-24 16:37:45 -0400

    Secciones faltantes del home

--- src/layouts/LayoutLadingPage.jsx
+++ src/layouts/LayoutLadingPage.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom"
 import CustomFooter from "../components/CustomFooter"
-
+import NavBar from "../components/navbar"
 
 const LayoutLadingPage = () => {
   return (
     <div className="mx-auto min-h-screen flex flex-col">
-      <h2>Navbar</h2>
+      <NavBar/>
       <main className="flex-1">
         <Outlet/>
       </main>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,15 @@
+import Presentacion from "../../../../telemedica/src/comp/presentacion"
+import Profesionales from "../../../../telemedica/src/comp/profesionales"
+import Servicios from "../../../../telemedica/src/comp/servicios"
 import ShowDoctors from "./components/ShowDoctors"
 import Testimonials from "./components/Testimonials/Testimonials"
-
 
 const Home = () => {
   return (
     <>
+    <Presentacion/>
+    <Profesionales/>
+    <Servicios/>
     <Testimonials/>
     <ShowDoctors/>
     </>
